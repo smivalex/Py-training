6c054fbb38970aafc06da30e411214e9de74d5b7
Ivan <124583858+smivalex@users.noreply.github.com>
2023-03-02 11:46:09 +0300

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,20 @@
+for countdown in 1,2,3,4,5,"hey!":
+    print(countdown)
+
+spells = [
+    "Riddikulus!",
+    "Wingardium Leviosa",
+    "Especto Patronum",
+    "Lumos"
+]
+print(spells[0])
+for i in spells:
+    print(i)
+
+quotes = {
+    "Moe" : "A wise gue, hyh?",
+    "Larry" : " Ow!",
+    "Curly" : "Nyuk nuyk!"
+}
+stooge = "Curly"
+print(stooge, "says:", quotes[stooge])
